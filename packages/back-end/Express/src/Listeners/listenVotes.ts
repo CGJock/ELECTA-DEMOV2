0d931280803c2cee47381fc720406772315e1776
CGJock <cjocksanest@gmail.com>
2025-07-17 09:08:12 -0600

    implementacion nuevos sockets en el frontend

--- packages/back-end/Express/src/Listeners/listenVotes.ts
+++ packages/back-end/Express/src/Listeners/listenVotes.ts
@@ -1,31 +1,29 @@
 import { Pool } from 'pg';
 import { Server } from 'socket.io';
+import { setLatestVoteData } from './voteCache.js';
 
 export async function listenToVotesChanges(pool: Pool, io: Server) {
   const client = await pool.connect();
   await client.query('LISTEN votes_channel');
 
-function toPercent(value: number, total: number): number {
-  return total ? parseFloat(((value / total) * 100).toFixed(2)) : 0;
-}
-
- client.on('notification', (msg) => {
-  try {
-    const data = JSON.parse(msg.payload!); 
-    io.emit('full-vote-data', {
-      ...data,//original data 
-      nullPercentage: toPercent(data.nullVotes, data.totalVotes),
-      blankPercentage: toPercent(data.blankVotes, data.totalVotes),  //parese data to float %
-      validPercentage: toPercent(data.validVotes, data.totalVotes),
-    });
-    console.log(`Payload recibido de ${msg.channel}: ${msg.payload}`);
-  } catch (e) {
-    console.error('Error al procesar payload de NOTIFY:', e);
+  function toPercent(value: number, total: number): number {
+    return total ? parseFloat(((value / total) * 100).toFixed(2)) : 0;
   }
-  io.emit('last-update'), (msg), msg
 
-});
-
-
-  console.log('Listening NOTIFY in  votes_channel');
-}
+  client.on('notification', (msg) => {
+    try {
+      const data = JSON.parse(msg.payload!);
+      const enhancedData = {
+        ...data,
+        nullPercentage: toPercent(data.nullVotes, data.totalVotes),
+        blankPercentage: toPercent(data.blankVotes, data.totalVotes),
+        validPercentage: toPercent(data.validVotes, data.totalVotes),
+      };
+      setLatestVoteData(enhancedData);
+      console.log(`payload`,msg)
+      io.emit('full-vote-data', enhancedData);
+    } catch (e) {
+      console.error('Error processing NOTIFY payload:', e);
+    }
+  })
+  };
