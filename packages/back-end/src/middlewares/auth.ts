7885ceab55db5a40e1e25be1ffc7c4009edbe6f6
Gonzalo Javier Aune <gonzaloaune@gmail.com>
2025-08-13 22:13:15 +0100

    rollback

--- packages/back-end/src/middlewares/auth.ts
+++ packages/back-end/src/middlewares/auth.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import { JWT_CONFIG, JWTPayload } from '@config/jwt.config';
+import { JWT_CONFIG, JWTPayload } from '@config/jwt.config.js';
 
 export interface AuthenticatedRequest extends Request {
   admin?: JWTPayload;
