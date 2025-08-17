a504a0780e5c27ebf21a6ca05c7f79e35c206421
CGJock <cjocksanest@gmail.com>
2025-08-17 11:26:54 -0600

    Revert "delete cookis for gets in visibility"
    This reverts commit d0e4b5319b02be5a3f9f8fccb416074b07c3c4c0.

--- packages/front-end/src/services/componentVisibilityService.ts
+++ packages/front-end/src/services/componentVisibilityService.ts
@@ -30,6 +30,7 @@ class ComponentVisibilityService {
     try {
       const response = await fetch(`${API_BASE_URL}/api/component-visibility/phases`, {
         method: 'GET',
+        credentials: 'include', // Enviar cookies
         headers: {
           'Content-Type': 'application/json'
         }
@@ -47,6 +48,7 @@ class ComponentVisibilityService {
     try {
       const response = await fetch(`${API_BASE_URL}/api/component-visibility/active-phase`, {
         method: 'GET',
+        credentials: 'include',
         headers: { 'Content-Type': 'application/json' }
       });
       if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
